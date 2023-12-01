4feb62dff59dee95c836330b801f7be483dc7e62
Jakub Maciej Tkaczyk <s95595@pollub.edu.pl>
2023-12-01 17:19:50 +0100

    Add feature: auto updating Warehouse on create Document + fix

--- src/main/java/com/project/warehouse/service/WarehouseService.java
+++ src/main/java/com/project/warehouse/service/WarehouseService.java
@@ -1,12 +1,15 @@
 package com.project.warehouse.service;
 
 import com.project.warehouse.model.Warehouse;
+import com.project.warehouse.model.enums.DocumentType;
 import org.springframework.data.crossstore.ChangeSetPersister;
 
 import java.util.List;
 
 public interface WarehouseService {
     Warehouse findByItemId(long id) throws ChangeSetPersister.NotFoundException;
+
+    void addItemQuantity(long itemId, int quantity, DocumentType type) throws ChangeSetPersister.NotFoundException;
 
     List<Warehouse> getAll();
 }
--- src/main/java/com/project/warehouse/service/WarehouseServiceDefault.java
+++ src/main/java/com/project/warehouse/service/WarehouseServiceDefault.java
@@ -1,22 +1,50 @@
 package com.project.warehouse.service;
 
+import com.project.warehouse.model.Item;
 import com.project.warehouse.model.Warehouse;
+import com.project.warehouse.model.enums.DocumentType;
 import com.project.warehouse.repository.WarehouseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 
 @Service
 public class WarehouseServiceDefault implements WarehouseService {
     @Autowired
-    WarehouseRepository warehouseRepository;
+    private WarehouseRepository warehouseRepository;
+
+    @Autowired
+    private ItemService itemService;
 
     @Override
-    public Warehouse findByItemId(long id) throws ChangeSetPersister.NotFoundException {
+    public Warehouse findByItemId(long id) throws NotFoundException {
         return warehouseRepository.findByItem_Id(id)
-                .orElseThrow(ChangeSetPersister.NotFoundException::new);
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @Override
+    public void addItemQuantity(long itemId, int quantity, DocumentType type) throws NotFoundException {
+        Optional<Warehouse> warehouseRecord = warehouseRepository.findByItem_Id(itemId);
+        Item item = itemService.getSingleItem(itemId);
+        if (warehouseRecord.isEmpty()) {
+            Warehouse newRecord = new Warehouse(0, quantity, item);
+            warehouseRepository.save(newRecord);
+        } else {
+            Warehouse record = warehouseRecord.get();
+            long newQuantity = record.getQuantity();
+            if(type == DocumentType.PZ)
+                newQuantity += quantity;
+            else
+                newQuantity -= quantity;
+            if (newQuantity >= 0) {
+                record.setQuantity(newQuantity);
+            }
+            warehouseRepository.save(record);
+        }
     }
 
     @Override
